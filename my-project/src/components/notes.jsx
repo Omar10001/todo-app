2c9f1a8952481b578aa1ad9a4933e22feacfe465
Omar10001 <leoomaraa@gmail.com>
2023-01-21 21:49:13 +0300

    yay

--- my-project/src/components/notes.jsx
+++ my-project/src/components/notes.jsx
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 
 function Notes() {
   const [visible, setVisible] = useState(true);
+  const [checked, setChecked] = useState(false);
+
+  const checkBtn = () => {
+    setChecked((check) => !check)
+
+  }
+
+
 
   const removeElement = () => {
     setVisible((prev) => !prev);
@@ -15,20 +23,19 @@ function Notes() {
             <ion-icon name="close"></ion-icon>
           </button>
           <textarea
-            className=" bg-[#fff8df] block w-80 h-72 overflow-hidden resize-y min-h-[40px] p-2 focus:outline-none "
+            className={` bg-[#fff8df] block w-80 h-72 overflow-hidden resize-y min-h-[40px] p-2 focus:outline-none ${checked && 'line-through'}  `}
             role="textbox"
             placeholder="Take notes here"
           ></textarea>
           <div className="flex justify-between w-full items-center ">
-            <div className="flex gap-2">
+            <div className="flex gap-2 w-fit cursor-pointer items-center">
               <input
-                className="bg-transparent"
+                className="bg-black w-5 h-5 "
                 type="checkbox"
-                id="vehicle1"
-                name="vehicle1"
-                value="Bike"
+                value="Done"
+                onClick={checkBtn}
               />
-              <span>Done</span>
+              <span className="text-base font-medium ">Done</span>
             </div>
             <div className="rounded-full bg-[#d3cefe] h-4 w-4"></div>
           </div>
