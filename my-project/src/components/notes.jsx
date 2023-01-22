7b2878ad05ffe5906de720c56901f3fbc7812432
Omar10001 <leoomaraa@gmail.com>
2023-01-22 13:06:21 +0300

    yay3

--- my-project/src/components/notes.jsx
+++ my-project/src/components/notes.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
 
 function Notes() {
   const [visible, setVisible] = useState(true);
@@ -13,31 +14,36 @@ function Notes() {
   };
 
   return (
-    <div className={`${!visible && 'hidden' }`}>
+    <div className={`${!visible && "hidden"}`}>
       {visible && (
         <div className="bg-[#fff8df] h-fit w-fit px-4 py-2 rounded ">
           <button className="w-fit" onClick={removeElement}>
             <ion-icon name="close"></ion-icon>
           </button>
-          <textarea
-            className={`bg-[#fff8df] block w-80 font-medium h-72 overflow-hidden resize-y min-h-[40px] p-2 focus:outline-none ${
-              checked && "line-through text-red-500"
-            }`}
-            role="textbox"
-            placeholder="Take notes here"
-            disabled={checked}
-          ></textarea>
+          <div className="my-4">
+            <TextField
+              className={`bg-[#fff8df] block w-80 h-fit overflow-hidden  min-h-[40px] p-2 focus:outline-none ${
+                checked && "line-through text-red-500"
+              }`}
+              multiline
+              role="textbox"
+              placeholder="Take notes here"
+              variant="standard"
+              disabled={checked}
+              maxRows={7}
+            />
+          </div>
           <div className="flex justify-between w-full items-center ">
             <div className="flex gap-2 w-fit cursor-pointer items-center">
               <input
-                className="bg-black w-5 h-5 "
+                className="bg-transparent rounded-full w-5 h-5 focus:ring-transparent border-blue-500 "
                 type="checkbox"
                 value="Done"
                 onClick={checkBtn}
               />
-              <span className="text-base font-medium ">Done</span>
+              <span className="text-base text-blue-500 mt-0.5 font-regular ">Done</span>
             </div>
-            <div className="rounded-full bg-[#d3cefe] h-4 w-4"></div>
+
           </div>
         </div>
       )}
--- my-project/tailwind.config.cjs
+++ my-project/tailwind.config.cjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './src/App.jsx',
-    './src/components/navBar.jsx',
-    './src/components/notes.jsx'
+    "./index.html",
+    "./src/App.jsx",
+    "./src/components/navBar.jsx",
+    "./src/components/notes.jsx",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
