9084f10c99fea7733c62959a9005664b0ea2ac55
Omar10001 <leoomaraa@gmail.com>
2023-01-22 11:53:49 +0300

    commit

--- my-project/src/components/notes.jsx
+++ my-project/src/components/notes.jsx
@@ -5,27 +5,27 @@ function Notes() {
   const [checked, setChecked] = useState(false);
 
   const checkBtn = () => {
-    setChecked((check) => !check)
-
-  }
-
-
+    setChecked((check) => !check);
+  };
 
   const removeElement = () => {
     setVisible((prev) => !prev);
   };
 
   return (
-    <span >
+    <span>
       {visible && (
         <div className="bg-[#fff8df] h-fit w-fit px-4 py-2 rounded">
           <button className="w-fit" onClick={removeElement}>
             <ion-icon name="close"></ion-icon>
           </button>
           <textarea
-            className={` bg-[#fff8df] block w-80 h-72 overflow-hidden resize-y min-h-[40px] p-2 focus:outline-none ${checked && 'line-through'}  `}
+            className={` bg-[#fff8df] block w-80 font-medium h-72 overflow-hidden resize-y min-h-[40px] p-2 focus:outline-none ${
+              checked && "line-through text-red-500"
+            }  `}
             role="textbox"
             placeholder="Take notes here"
+            disabled={checked}
           ></textarea>
           <div className="flex justify-between w-full items-center ">
             <div className="flex gap-2 w-fit cursor-pointer items-center">
