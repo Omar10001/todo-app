8c34aebaba3512bef172a88e6b3685d9d1bee078
Omar10001 <leoomaraa@gmail.com>
2023-01-25 21:06:58 +0300

    add loader

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import NavBar from "./components/navBar";
 import Notes from "./components/notes";
@@ -8,8 +8,10 @@ import useLocalStorage from "./hooks/useLocalStorage";
 // import EditForm from "./components/EditForm";
 
 import Dialog from "@mui/material/Dialog";
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [tasks, setTasks] = useLocalStorage("react-todo.tasks", []);
   const [open, setOpen] = useState(false);
 
@@ -83,43 +85,60 @@ function App() {
     setTasks(newItem);
   };
 
-  return (
-    <div className="App w-full h-full flex gap-9">
-      <NavBar filterItem={filterItem} setTasks={setTasks} category={category} />
-      <div className="bg-[#fffefe] min-h-screen h-full w-full flex flex-col  ">
-        <div className="w-full flex justify-end items-center text-4xl md:p-9 px-9 pb-6 pt-9 ">
-          <button onClick={handleClickOpen} className="w-fit">
-            <ion-icon name="add"></ion-icon>
-          </button>
-        </div>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          className="bg-transparent/40 backdrop-blur-sm transition-all duration-150"
-        >
-          <CustomForm
-            category={category}
-            addTask={addTask}
-            handleClose={handleClose}
-          />
-        </Dialog>
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 400);
+  }, []);
 
-        {/* TaskLIST */}
-        {tasks && (
-          <div className="flex flex-wrap h-full w-full gap-2 md:gap-3">
-            {tasks
-              .sort((a, b) => b.id - a.id)
-              .map((task) => (
-                <Notes
-                  key={task.id}
-                  task={task}
-                  deleteTask={deleteTask}
-                  toggleTask={toggleTask}
-                />
-              ))}
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen">
+      {loading ? (
+        <HashLoader color="#ffc806" speedMultiplier={2} />
+      ) : (
+        <div className="App w-full h-full flex gap-9">
+          <NavBar
+            filterItem={filterItem}
+            setTasks={setTasks}
+            category={category}
+          />
+          <div className="bg-[#fffefe] min-h-screen h-full w-full flex flex-col  ">
+            <div className="w-full flex justify-end items-center text-4xl md:p-9 px-9 pb-6 pt-9 ">
+              <button onClick={handleClickOpen} className="w-fit">
+                <ion-icon name="add"></ion-icon>
+              </button>
+            </div>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              className="bg-transparent/40 backdrop-blur-sm transition-all duration-150"
+            >
+              <CustomForm
+                category={category}
+                addTask={addTask}
+                handleClose={handleClose}
+              />
+            </Dialog>
+
+            {/* TaskLIST */}
+            {tasks && (
+              <div className="flex flex-wrap h-full w-full gap-2 md:gap-3">
+                {tasks
+                  .sort((a, b) => b.id - a.id)
+                  .map((task) => (
+                    <Notes
+                      key={task.id}
+                      task={task}
+                      deleteTask={deleteTask}
+                      toggleTask={toggleTask}
+                    />
+                  ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
